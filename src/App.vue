a323336946ba7cd6a4c38457ace310a2115df9d4
Akhil Sai <akhil@pop-os.localdomain>
2021-06-22 15:08:25 +0530

    implemented delete button

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,63 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h2 class="h2 text-center mt-5">To Do</h2>
+    <div class="d-flex">
+      <input type="text" v-model="task" name="task" id="task" class="form-control rounded-0">
+      <button @click="submitTask" class="btn btn-warning rounded-0">Submit</button>
+    </div>
+    <table class="table table-bordered mt-5">  
+  <thead>
+    <tr>
+      <th scope="col">Task</th>
+      <th scope="col">Status</th>
+      <th scope="col">Edit</th>
+      <th scope="col">Delete</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="(task,index) in tasks" :key="index">
+      <td ><span>{{ task.name }}</span></td>
+      <td>{{task.status}}</td>
+      <td>
+        <div class="text-center"> 
+          <span class="fa fa-pen"></span>
+        </div>
+        </td>
+      <td><div class="text-center" @click="deleteTask(index)"> 
+          <span class="fa fa-trash"></span>
+        </div>
+        </td>
+    </tr>
+  </tbody>
+</table>
+  </div>  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      task: "",
+    tasks: []
+    }
+  },
+  methods: {
+    submitTask(){
+      if(this.task.length === 0){
+        alert("enter the task")
+      }
+      this.tasks.push({
+        name: this.task,
+        status: "To-Do"
+      })
+      this.task = " "
+    },
+    deleteTask(index){
+      this.tasks.splice(index, 1)
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
